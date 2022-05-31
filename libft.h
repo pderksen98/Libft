1323c329901c1b9a0bd496cd0e264f9063ae6f45
Pieter Derksen <pieterderksen@MacBook-Pro-van-Pieter.local>
2022-05-31 16:13:55 +0200

    header file -- good version

--- libft.h
+++ libft.h
@@ -65,5 +65,12 @@ char	*ft_strdup_c(char *line, char c);
 void	ft_check_malloc(void *ptr);
 int		ft_split_len(char **line_2d);
 void	ft_split_free(char **line_2d);
-
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstlast(t_list *lst);
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list	*ft_lstnew(void *content);
 #endif
